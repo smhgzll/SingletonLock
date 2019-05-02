b8015b9ac1df13df7101ae45aa2e1a717a3ff0c6
Semih Güzelel <semihguzelel@gmail.com>
2019-05-02 16:25:22 +0300

    Add project files.

---
+++ SingletonLock/DumbRepo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonLock
+{
+    public class DumbRepo
+    {
+        private static DumbRepo _repo;
+
+        private static object LockObject = new object();
+
+        private static int CallCounter;
+
+        public static DumbRepo Instance
+        {
+            get
+            {
+                lock (LockObject)
+                {
+                    if (_repo == null)
+                    {
+                        _repo = new DumbRepo();
+                        CallCounter = 1;
+                        Console.WriteLine(CallCounter + " : " + nameof(DumbRepo) + " object is created.");
+                    }
+                    Console.WriteLine(CallCounter + " : " + "An existing " + nameof(DumbRepo) + " object is send.");
+                    Console.WriteLine();
+                    CallCounter += 1;
+                    return _repo;
+                }
+            }
+        }
+
+    }
+}
---
+++ SingletonLock/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonLock
+{
+    class Program
+    {
+        delegate void AsyncHandler();
+
+        static void Main(string[] args)
+        {
+            AsyncHandler method = () => {
+                var repo = DumbRepo.Instance; // Instantiate or call DumbRepo object;
+            };
+            
+            Task.Run(() => { method(); }); // Call 1;
+            Task.Run(() => { method(); }); // Call 2;
+            Task.Run(() => { method(); }); // Call 3;
+            Task.Run(() => { method(); }); // Call 4;
+            Task.Run(() => { method(); }); // Call 5;
+            Task.Run(() => { method(); }); // Call 6;
+
+            Console.Read();
+        }
+    }
+}
